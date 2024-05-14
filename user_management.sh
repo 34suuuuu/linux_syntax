ea28a435197911fee11dcb2b502a1d3e19c4b51b
34suuuuu <psh7492@gmail.com>
2024-05-14 10:47:32 +0900

    사용자와 그룹 및 권한 관리

---
+++ user_management.sh
@@ -0,0 +1,17 @@
+# 사용자 추가
+sudo useradd 사용자명
+# 패스워드 지정
+sudo passwd 사용자명 
+# 사용자 목록 조회
+cat /etc/passwd
+
+# 사용자 전환 : 비밀번호는 전환하고자 하는 사용자 비밀번호
+su - 사용자명
+
+# 파일 권한 관리
+# 권한은 3개의 파트 (user:group:others)로 구성되어있고, 각 권한은 r(4)w(2)x(1)
+chmod 777(숫자) test.sh
+chmod u+x test.sh
+chmod g-w test.sh
+
+# 파일 소유자/그룹 관리
